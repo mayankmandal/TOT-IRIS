39625b5b1204bab1e5501f89c78e2be3272da872
mayankmandal <mayankmandal077@gmail.com>
2023-09-18 01:29:12 +0530

    Add project files.

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php include "header.php"; ?>
+<div class="row" style="margin-top: -20px">
+
+	<!-- Carousel -->
+	<div id="carousel-hello" class="carousel slide" data-ride="carousel" data-interval="2000">
+		
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-hello" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel-hello" data-slide-to="1"></li>
+			<li data-target="#carousel-hello" data-slide-to="2"></li>
+			<li data-target="#carousel-hello" data-slide-to="3"></li>
+			<li data-target="#carousel-hello" data-slide-to="4"></li>
+		</ol>
+		
+		<div class="carousel-inner">
+			<div class="item active">
+				<img src="images/1.jpg" alt="First slide" class="img-responsive">
+			</div>
+
+			<div class="item">
+				<img src="images/2.png" alt="Second slide" class="img-responsive">
+			</div>
+			<div class="item">
+				<img src="images/3.png" alt="Second slide" class="img-responsive">
+			</div>
+			<div class="item">
+				<img src="images/4.png" alt="Second slide" class="img-responsive">
+			</div>
+			<div class="item">
+				<img src="images/5.png" alt="Second slide" class="img-responsive">
+			</div>
+		</div>
+				
+				<a class="carousel-control left" href="#carousel-hello" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+				</a>
+				<a class="carousel-control right" href="#carousel-hello" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+				</a>
+				</div>
+			</div>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h3><u>TOT IRIS</u></h3><br>
+						<p style="text-align: justify; font-size: 17px;"> “TOT IRIS” provides us a simple interface for maintenance of student information. It can be used by educational institutes or colleges to maintain the records of students easily. Achieving this objective is difficult using a manual system as the information is scattered, can be redundant and collecting relevant information may be very time consuming. All these problems are solved using this project.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<h3><u>TOT IRIS Supports:</u></h3><br>
+						<ul class="project-support">
+							<li>Handling inquiries from prospective students.</li>
+							<li>Handling the Student details.</li>
+							<li>Maintaining the Student Marks Details.</li>
+							<li>Maintaining records of Internal marks.</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<?php include "footer.php"; ?>
